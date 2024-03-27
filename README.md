9d67feb6f57434b6ef187f61884f16be23148827
odunsikazeem <145531096+odunsikazeem@users.noreply.github.com>
2024-03-27 15:36:43 +0000

    Folder restructure

---
+++ README.md
@@ -0,0 +1,19 @@
+# React-Portfolio-Page
+
+## Description
+This work is showcasing how far i have gone since i started frontend development last year.It contains a portfolio of my work.It was beautifully crafted using React because of its modularization tendercies.This is still an ongoing project that will be regularly updated when the finished product is out.
+
+## Installation
+N/A
+
+## Usage
+The web application is for public viewing and a call to collaborate with fellow developers.Feel free to peruse my work and comments are also welcome for improvement.
+
+<img src="./portfolio/src/asset/portfolio.png" alt="image">
+
+
+## Credit
+Special appreciation goes to my tutor Omar of edX and Sadiq Odunsi(Adventrum) for their encouragement and moral support.
+
+Lincence
+N/A
---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Nav/Navbar';
+import Homepage from './components/pages/home-folder/Homepage';
+import Contact from './components/pages/contact-folder/Contact';
+import Projectspage from './components/pages/projectpage-folder/Projectspage';
+import Footer from './components/Footer/Footer';
+import About from './components/pages/aboutme/About';
+import Skills from './components/Skills/Skills';
+
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="projectspage/*" element={<Projectspage />} />
+        <Route path="contact/*" element={<Contact />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+      
+      <Footer/>
+    </Router>
+    
+  );
+}
+
+export default App;
